ee8ba63ae627ddedbbe09990b809c104f7ca28bb
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2021-02-13 19:35:22 -0600

    extract framework

--- site/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/filippofonseca/Desktop/Developer/Work/Zyndicate/mathiumjs/site/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/filippofonseca/Desktop/Developer/Work/Zyndicate/mathiumjs/site/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/filippofonseca/Desktop/Developer/Work/Zyndicate/mathiumjs/site/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/filippofonseca/Desktop/Developer/Work/Zyndicate/mathiumjs/site/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/filippofonseca/Desktop/Developer/Work/Zyndicate/mathiumjs/site/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/filippofonseca/Desktop/Developer/Work/Zyndicate/mathiumjs/site/src/css/custom.css"),
-];
